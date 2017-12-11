c73a75bb5667aa11fb64e296466e6433332e5ab0
nguyennhattam <sun.nguyen.1506@gmail.com>
2017-12-11 10:52:38 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,68 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-room_update",
+      "name": "room_update",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+	          {
+			"id":"bl-dex",
+          "yaw": 0.15817079067951667,
+          "pitch": 0.5932832353643178,
+          "title": "Dex",
+          "text": "Text"
+        },
+		        {
+			"id":"bl-gear-vr",
+          "yaw": -2.8608996491794585,
+          "pitch": 0.40623800938919175,
+          "title": "Gear VR<br>",
+          "text": "Text"
+        },
+        {
+			"id":"bl-gear-360",
+          "yaw": 0.9911480344936408,
+          "pitch": 0.6524414394224944,
+          "title": "Gear 360<br>",
+          "text": "Text"
+        },
+        {
+		  "id":"bl-gear-s3", 
+          "yaw": 2.9718409143747175,
+          "pitch": 1.212857575709707,
+          "title": "Gear S3",
+          "text": "Text"
+        }
+
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
